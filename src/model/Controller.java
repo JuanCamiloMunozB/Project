62d02a7ccd14c8065da4407515d880b4a94167f7
Juan Camilo Muñoz Barco <1105363626@u.icesi.edu.co>
2023-03-31 23:07:29 -0500

    requirements implementation

--- src/model/Controller.java
+++ src/model/Controller.java
@@ -5,35 +5,78 @@ import java.util.GregorianCalendar;
 
 public class Controller {
 
+	private static final int MAX_PROYECTS = 10;
+	private int projectCounter;
 	private Project[] projects;
 
 	public Controller() {
 
-		projects = new Project[10];
+		projects = new Project[MAX_PROYECTS];
 	
 	}
 	
-	//Incomplete
-	public boolean RegisterProject() {
+	public boolean isRegistrableProyect(){
+		if(projectCounter == MAX_PROYECTS){
+			return false;
+		}else{
+			return true;
+		}
+	}
+
+
+	public boolean registerProject(String projectName, String clientName, String type, Calendar initialDate, Calendar finalDate, double budget){
+		
+		Project project = new Project(projectName, clientName, type, initialDate, finalDate, budget);
+		int pos = getFirstValidPosition();
+		if(pos != -1){
+			projects[pos]= project;
+		}
+		projectCounter++;
 
 		return false;
 	}
 
-	//Incomplete
-	// Date class also has their own before() and after() method
-	public String searchProjectsAfterDate() {
+	public int getFirstValidPosition(){
+		int pos = -1; 
+		boolean isFound = false; 
+		for(int i = 0; i < MAX_PROYECTS && !isFound; i++){
+			if(projects[i] == null){
+				pos = i; 
+				isFound = true;
+			}
+		}
+		return pos; 
+	}
 
-		String msg = "";
+	
+	public String searchProjectsAfterDate(Calendar date) {
+
+		String msg = ("There is no proyect that ends before: "+date.getTime());
+
+		for(int i = 0; i<MAX_PROYECTS; i++){
+			if(projects[i] != null){
+				if(date.compareTo(projects[i].getFinalDate())>0){
+					msg += ("Proyect's name: "+projects[i].getName());
+				}
+			}
+		}
 
 		return msg;
 
 	}
 	
-	//Incomplete
-	// Date class also has their own before() and after() method
-	public String searchProjectsBeforeDate() {
+	
+	public String searchProjectsBeforeDate(Calendar date) {
 
-		String msg = "";
+		String msg = "There is no proyect that starts after: "+date.getTime();
+
+		for(int i = 0; i<MAX_PROYECTS; i++){
+			if(projects[i] != null){
+				if(date.compareTo(projects[i].getFinalDate())<0){
+					msg += ("Proyect's name: "+projects[i].getName());
+				}
+			}
+		}
 
 		return msg;
 
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -3,41 +3,189 @@ package ui;
 import java.util.Scanner;
 import model.Controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import java.text.ParseException;
+
 public class Main{
 
 	private Scanner reader;
 	private Controller controller;
+	private SimpleDateFormat format;
 
 	public Main() {
 
 		reader = new Scanner(System.in);
 		controller = new Controller();
+		format = new SimpleDateFormat("dd/M/yy");
 	}
 
 	public static void main(String[] args) {
 
 		Main exe = new Main();
-		exe.menu();
+		int answer; 
 
+		do{
+			exe.menu();
+			answer = exe.validateIntegerInput();
+			exe.executeOption(answer);
+		}while(answer != 0);
 	}
 
 	// Incomplete
 	public void menu() {
+		System.out.println("0. Exit");
+		System.out.println("1. Register new Project");
+		System.out.println("2. Consult Projects that end before a certain date");
+		System.out.println("3. Consult projects that start after a date");
+	}
 
+	public void executeOption(int ans){
+		switch(ans){
+			case 0:
+				System.out.println("Thanks for using our app!");
+			break;
+
+			case 1:
+				registerProject();
+			break;
+
+			case 2:
+				searchProjectsAfterDate();
+			break;
+
+			case 3:
+				searchProjectsBeforeDate();
+			break;
+
+			default:
+			System.out.println("Invalid option. Please, try again. ");
+		}
 	}
 
 	//Incomplete
-	public void RegisterProject() {
+	public void registerProject(){
+		String projectName;
+		String clientName;
+		String type;
+		Calendar initialDate;
+		Calendar finalDate;
+		double budget;
 
+		boolean isProyectAvailable = controller.isRegistrableProyect();
+
+		if(isProyectAvailable == false){
+			System.out.println("The maximun amount of proyects posible has alredy been reached");
+		}else{
+			System.out.print("Type the project's name: ");
+			projectName = reader.nextLine();
+			reader.next();
+
+			System.out.print("Type the client's name: ");
+			clientName = reader.nextLine();
+			reader.next();
+
+			type = validateProjectsType("\nType the project's type ");
+
+			initialDate = readDate("\nType the start date of the proyect");
+			finalDate = readDate("\nType the end date of the proyect");
+			
+			System.out.print("\nType the project's budget: ");
+			budget = reader.nextDouble();
+
+			controller.registerProject(projectName, clientName, type, initialDate, finalDate, budget);
+		}
 	}
 
-	//Incomplete
+
 	public void searchProjectsAfterDate() {
 
+		Calendar deadLine = readDate("Enter the deadline");
+		System.out.println(controller.searchProjectsAfterDate(deadLine));		
 	}
 	
-	//Incomplete
+	
 	public void searchProjectsBeforeDate() {
+		Calendar foreCast = readDate("Enter the Forecasts");
 
+		controller.searchProjectsBeforeDate(foreCast);	
 	}
+
+
+	public void typeMenu(){
+		System.out.println("valid options:");
+		System.out.println("1. Development");
+		System.out.println("2. maintenance");
+		System.out.println("3. Deployment");
+	}
+
+	public String validateProjectsType(String msg){
+
+		String type = "";
+		int answ = 0;
+
+		do{
+			System.out.println(msg);
+			typeMenu();
+			System.out.print(":");
+			answ = validateIntegerInput();
+
+			if(answ<1 || answ>3){
+				System.out.println("Invalid Option. Please, try again");
+			}else{
+				switch(answ){
+					case 1:
+					type = "Development";
+					break;
+
+					case 2:
+					type = "Maintenance";
+					break;
+
+					case 3:
+					type = "Deployment";
+				}
+			}
+		}while(type == "");
+		return type;
+	}
+
+	public Calendar readDate(String msg){
+		
+		Calendar calendarTime = new GregorianCalendar();
+		String date = "";
+		boolean isDateSet;
+		do{
+		isDateSet = false;
+		System.out.println(msg); 
+		System.out.println("The date must follow the format: dd/M/yy");
+		System.out.print("Type the date: ");
+		date = reader.next();
+		
+		try {
+			calendarTime.setTime(format.parse(date));
+			isDateSet= true;
+		} catch (ParseException error) {
+			System.out.println("Invalid option. Please, try again");
+		}
+		}while(isDateSet == false);
+		
+		return calendarTime;
+	}
+
+	public int validateIntegerInput(){
+        int option; 
+        if(reader.hasNextInt()){
+            option = reader.nextInt();
+        }
+        else{
+            reader.nextLine();
+			option = -1;
+            System.out.print("Enter an intenger value. "); 
+        }
+        return option; 
+    }
+	
 }
